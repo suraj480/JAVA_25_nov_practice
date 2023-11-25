4e6cbaf2af26df600b9934707ba5700ab118a1fd
suraj480 <surajmaurya991@gmail.com>
2023-11-25 16:15:46 +0530

    2-D array

---
+++ Array.java
@@ -0,0 +1,19 @@
+public class Array {
+    public static void main(String[] args) {
+        int nums[][] = new int[3][4];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                nums[i][j] = (int) (Math.random()*100);
+                 System.out.println(nums[i][j] + " ");
+            }
+
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
